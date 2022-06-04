d884476e369ce297e7789c6819e91869d918815c
sikethedev <ivoinestrachan242@gmail.com>
2022-06-04 11:21:57 -0400

    eh

---
+++ client/pages/login.js
@@ -0,0 +1,20 @@
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" placeholder="username"/>
+        </div>
+        <div>
+          <input type="password" placeholder="password"/>
+        </div>
+        <button>login</button>
+      </form>
+
+      
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/pages/signup.js
@@ -0,0 +1,21 @@
+
+const Signup = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" placeholder="username"/>
+        </div>
+        <div>
+          <input type="password" placeholder="password"/>
+        </div>
+        <div>
+          <input type="password" placeholder="comfirm password"/>
+        </div>
+        <button>signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
